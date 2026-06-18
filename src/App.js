d638a07af34b013f7b01427e33ab44eb78a347ed
riyaann422 <riyaannmathew2028@cs.sjcetpalai.ac.in>
2026-06-18 21:48:12 +0530

    Initial React Todo App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>My Todo App</h1>
+      <input type="text" placeholder="Enter a task" />
+      <button>Add Task</button>
+    </div>
+  );
+}
+
+export default App;
